a993da2a5f9405f638d3595208524405160be6de
Adam Rushford <177971875+Autodidac@users.noreply.github.com>
2025-11-07 03:30:34 -0500

    Fix test registration and brighten marching cubes shading

--- tests/test_sources.cmake
+++ tests/test_sources.cmake
@@ -1,7 +1,6 @@
-include_guard()
-
 set(ALMOND_VOXEL_TEST_CASE_SOURCES
-    "${CMAKE_CURRENT_LIST_DIR}/chunk_tests.cpp"
-    "${CMAKE_CURRENT_LIST_DIR}/meshing_tests.cpp"
-    "${CMAKE_CURRENT_LIST_DIR}/serialization_tests.cpp"
+    ${CMAKE_CURRENT_LIST_DIR}/chunk_tests.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/meshing_tests.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/serialization_tests.cpp
+    CACHE INTERNAL "Almond Voxel unit test sources"
 )
